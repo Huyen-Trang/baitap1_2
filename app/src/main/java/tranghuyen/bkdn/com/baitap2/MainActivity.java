e5592d20ede1b53d4e1fb031d261d7455e4bf82d
Huyen-Trang <huyentranghathi@gmail.com>
2018-02-03 21:46:08 +0700

    first commit

---
+++ app/src/main/java/tranghuyen/bkdn/com/baitap2/MainActivity.java
@@ -0,0 +1,13 @@
+package tranghuyen.bkdn.com.baitap2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.huyentrang_baitap2_mh1);
+    }
+}
